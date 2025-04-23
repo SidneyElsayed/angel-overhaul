21b6ed353ee014e5be39c977e9c1336993539ffa
SidneyElsayed <154788807+SidneyElsayed@users.noreply.github.com>
2025-04-24 02:10:52 +0300

    slick slider homepage WITH BUGS
    BUGS:
    - slick-track is long? giving extra space to the right
    - dots off center
    - images won't self-center
    - images are actually taller than the max-height now, so logo name is being cut off

--- main.js
+++ main.js
@@ -24,3 +24,39 @@ for (i = 0; i < acc.length; i++) {
   });
 } 
 
+// Slick Slider
+$('.slider-container').slick({
+	centerMode: false, // Makes the center photo slightly bigger
+    centerPadding: '60px', // Padding for the photos
+    slidesToShow: 5, // Self explainatory
+    autoplay: true, // Do we want it to autoplay? true or false
+	autoplaySpeed: 3000, // How long between each slide when auto-playing
+	speed: 1000, // How fast is the transition in milliseconds
+	arrows: true, // Do you want to show arrows to trigger each slide
+	accessibility: true, // Enables keyboard tabbing and arrow key navigation
+	dots: true, // Enables the dots below to show how many slides
+	fade: false, // Changes the animate from slide to fade if true
+	infinite: true, // When true, means that it will scroll in a circle
+	pauseOnHover: true, // When true means the autoplay pauses when hovering
+	pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
+	responsive: [
+		{
+		  breakpoint: 768,
+		  settings: {
+			arrows: true,
+			centerMode: true,
+			centerPadding: '40px',
+			slidesToShow: 2
+		  }
+		},
+		{
+		  breakpoint: 576,
+		  settings: {
+			arrows: true,
+			centerMode: true,
+			centerPadding: '40px',
+			slidesToShow: 1
+		  }
+		}
+	  ]
+});
