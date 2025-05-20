0a6072170e8e56b8fae2aa2906e0258c87a4b82c
SidneyElsayed <154788807+SidneyElsayed@users.noreply.github.com>
2025-05-20 16:12:08 +0300

    pushing updates

--- main.js
+++ main.js
@@ -24,7 +24,24 @@ for (i = 0; i < acc.length; i++) {
   });
 } 
 
-// Slick Slider
+// Testimonial Slick Slider
+$('.slider-testimonial').slick({
+	centerMode: false, // Makes the center photo slightly bigger
+    centerPadding: '60px', // Padding for the photos
+    slidesToShow: 1, // Self explainatory
+    autoplay: true, // Do we want it to autoplay? true or false
+	autoplaySpeed: 4000, // How long between each slide when auto-playing
+	speed: 1000, // How fast is the transition in milliseconds
+	arrows: true, // Do you want to show arrows to trigger each slide
+	accessibility: true, // Enables keyboard tabbing and arrow key navigation
+	dots: true, // Enables the dots below to show how many slides
+	fade: false, // Changes the animate from slide to fade if true
+	infinite: true, // When true, means that it will scroll in a circle
+	pauseOnHover: true, // When true means the autoplay pauses when hovering
+	pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
+});
+
+// Logo Slick Slider
 $('.slider-container').slick({
 	centerMode: false, // Makes the center photo slightly bigger
     centerPadding: '60px', // Padding for the photos
@@ -34,7 +51,7 @@ $('.slider-container').slick({
 	speed: 1000, // How fast is the transition in milliseconds
 	arrows: true, // Do you want to show arrows to trigger each slide
 	accessibility: true, // Enables keyboard tabbing and arrow key navigation
-	dots: true, // Enables the dots below to show how many slides
+	dots: false, // Enables the dots below to show how many slides
 	fade: false, // Changes the animate from slide to fade if true
 	infinite: true, // When true, means that it will scroll in a circle
 	pauseOnHover: true, // When true means the autoplay pauses when hovering
